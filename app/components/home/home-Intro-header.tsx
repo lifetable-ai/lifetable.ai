d2c38caeec016a3bdfc66391aeabcd45bec44a73
hylerrix <hylerrix@gmail.com>
2024-05-14 10:23:21 +0800

    chore: 2024-05-14 pick up, ready for next round

--- app/components/home/home-Intro-header.tsx
+++ app/components/home/home-Intro-header.tsx
@@ -3,7 +3,7 @@ import Image from "next/image"
 
 import { nFormatter } from "@/lib/utils"
 import { DEPLOY_URL } from "@/lib/constants"
-import { Github, Twitter } from "@/components/shared/icons"
+import { Github } from "@/components/shared/icons"
 import { SupportSsDB } from "./constant"
 import { SupportItem } from "./type"
 
@@ -138,7 +138,7 @@ export default function HomeIntroHeader() {
         <a className="inline-flex justify-center items-center gap-x-3 text-center bg-gradient-to-tl from-blue-600 to-violet-600 hover:from-violet-600 hover:to-blue-600 border-transparent text-white text-sm font-medium rounded-full focus:outline-none focus:ring-2 focus:ring-blue-600 focus:ring-offset-2 focus:ring-offset-white py-3 px-4 dark:focus:ring-offset-gray-800" href="/os/lifetable">
           Explore Now
           <svg className="w-3 h-3" width="16" height="16" viewBox="0 0 16 16" fill="none">
-            <path d="M5.27921 2L10.9257 7.64645C11.1209 7.84171 11.1209 8.15829 10.9257 8.35355L5.27921 14" stroke="currentColor" stroke-width="2" stroke-linecap="round"/>
+            <path d="M5.27921 2L10.9257 7.64645C11.1209 7.84171 11.1209 8.15829 10.9257 8.35355L5.27921 14" stroke="currentColor" strokeWidth="2" strokeLinecap="round"/>
           </svg>
         </a>
       </div>
